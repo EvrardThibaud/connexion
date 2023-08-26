a493ed8aeace3637f4518486b18b7513ee8fadc5
evti <obit2004@hotmail.com>
2023-08-26 19:09:55 +0200

    messages

--- js/scroll_message.js
+++ js/scroll_message.js
@@ -1,6 +1,4 @@
-// Récupérer la boîte de messages
 var messageBox = document.getElementById('messageBox');
 
-// Régler la position de défilement au bas par défaut
 messageBox.scrollTop = messageBox.scrollHeight;
-messageBox.classList.add('scrollToBottom');
+messageBox.classList.add('scrollSmooth');
